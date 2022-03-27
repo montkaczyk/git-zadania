7929a77138949da4dc0a80f641b3171e8a4e0a73
montkaczyk <tkaczykmonika@interia.pl>
2022-03-27 12:16:56 +0200

    1

--- program/Program.java
+++ program/Program.java
@@ -1,1 +1,16 @@
 class
+import java.util.ArrayList;
+
+public class Program {
+    public static void main(String[] args) {
+        //tworzenie listy obiektów klasy Auto
+        ArrayList<Auto> lista_aut = new ArrayList<>();
+        for(int i = 0; i<3;i++){
+            lista_aut.add(new Auto());
+        }
+        for(Auto i : lista_aut){
+            System.out.println(i.toString());
+        }
+
+    }
+}
